c9b9e31118689727d4d4390a9ae6ed9f9bbf7974
ohoraming <kminsmile@gmail.com>
2022-06-09 23:15:51 +0900

    db 설정 수정

--- config/settings.py
+++ config/settings.py
@@ -83,17 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'danbi',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
+DATABASES = secret_file.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- secret_file.py
+++ secret_file.py
@@ -1,1 +1,11 @@
 SECRET_KEY = "django-insecure-y=0=lc#u6#fonouq=2ha3w471uy@3yowbv#j6ik1n2qgqzw-zc"
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'routine',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
